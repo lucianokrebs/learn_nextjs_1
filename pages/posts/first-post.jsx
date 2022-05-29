4e445714ac33208b8aec8616ec2b12ec859bf908
Luciano Krebs <lkrebs05@gmail.com>
2022-05-29 12:53:02 -0300

    wip - start to add dynamic routes

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Script from 'next/script'
+import Layout from '../../components/layout';
+
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
@@ -20,6 +22,6 @@ export default function FirstPost() {
       <Link href="/">
         <a>Back to home</a>
       </Link>
-    </>
+    </Layout>
   )
 }
